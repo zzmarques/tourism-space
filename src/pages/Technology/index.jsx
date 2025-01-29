df38281054dc53da315f9cf5684600126a01acc5
zzmarques <walacemarqs@gmail.com>
2025-01-29 13:20:15 -0300

    css mobile finalizado da page technology

--- src/pages/Technology/index.jsx
+++ src/pages/Technology/index.jsx
@@ -17,7 +17,7 @@ const Technology = () => {
             setWidth(window.innerWidth)
         };
         window.addEventListener('resize', handleResize);
-       
+
         return () => {
             window.removeEventListener('resize', handleResize);
         };
@@ -53,39 +53,53 @@ const Technology = () => {
     return (
         <div className={styles.technology}>
             <Header/>
-            <Container>
-                <main className={styles.main}>
-                <h1><span>03</span> Space launch 101</h1>
-                    <section>
+            <section className={styles.container_tec}>
+                <Container>
+                    <div className={styles.main}>
+
+                        <h1><span>03</span> Space launch 101</h1>
+                        
                         <div className={styles.container}>
+                            <div className={styles.imgs}>
+                                <img src={width < 1024 ? imgLandscape : imgPortrait} alt="foto naves" />
+                            </div>
+
                             <div className={styles.content}>
+
                                 <div className={styles.btns}>
                                     <div 
                                         onClick={() => handleTec(0)} 
                                         className={tecInfo === ind[0] ? `${styles.show}` : `${styles.hiden}`}
-                                    >1</div>
+                                    >
+                                        <span>1</span>
+                                    </div>
                                     <div 
                                         onClick={() => handleTec(1)}
                                         className={tecInfo === ind[1] ? `${styles.show}` : `${styles.hiden}`}
-                                    >2</div>
+                                    >
+                                        <span>2</span>
+                                    </div>
                                     <div 
                                         onClick={() => handleTec(2)}
                                         className={tecInfo === ind[2] ? `${styles.show}` : `${styles.hiden}`}
-                                    >3</div>
+                                    >
+                                        <span>3</span>
+                                    </div>
                                 </div>
                                 <div className={styles.text}>
-                                    <span>The terminology...</span>
-                                    <h1>{content.name}</h1>
+                                    <div className={styles.rn}>
+                                        <span>The terminology...</span>
+                                        <h2>{content.name}</h2>
+                                    </div>
                                     <p>{content.description}</p>
                                 </div>
                             </div>
+
                         </div>
-                        <div className={styles.imgs}>
-                            <img src={width < 1024 ? imgLandscape : imgPortrait} alt="foto naves" />
-                        </div>
-                    </section>
-                </main>
-            </Container>
+
+                    </div>
+                </Container>
+            </section>
         </div>
     )
 }
